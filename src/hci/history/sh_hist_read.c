324d8213ac715ce21fe235e3ba255426d1e62f8e
pguillie <pguillie@student.42.fr>
2017-12-03 11:17:26 +0100

    FIX LEAKS global hist

--- src/hci/history/sh_hist_read.c
+++ src/hci/history/sh_hist_read.c
@@ -6,7 +6,7 @@
 /*   By: mdescamp <mdescamp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 20:54:56 by mdescamp          #+#    #+#             */
-/*   Updated: 2017/12/02 23:17:50 by ysan-seb         ###   ########.fr       */
+/*   Updated: 2017/12/03 11:09:17 by pguillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ t_line	*sh_hist_create(t_line *hist, char **line)
 	return (hist);
 }
 
-t_line	*sh_hist_gnl(int fd)
+t_line	**sh_hist_gnl(int fd)
 {
 	t_line	**hist;
 	char	*line;
@@ -59,38 +59,38 @@ t_line	*sh_hist_gnl(int fd)
 	while ((ret = get_next_line(fd, &gnl)) > 0)
 	{
 		if (!(line = sh_hist_line(line, gnl)))
-			return ((*hist));
+			return (hist);
 		if (!gnl[0] || !line[0] || line[ft_strlen(line) - 1] != '\n')
 		{
 			if (!((*hist) = sh_hist_create((*hist), &line)))
-				return ((*hist));
+				return (hist);
 		}
 		free(gnl);
 	}
 	if (ret < 0)
 		ft_error("Warning", "History not totally recovered", NULL);
-	return ((*hist));
+	return (hist);
 }
 
 t_line	*sh_hist_read(void)
 {
-	t_line			*hist;
-	t_line			*new;
-	int				fd;
+	t_line		**hist;
+	t_line		*new;
+	int			fd;
 
 	fd = open(sh_getvar("HISTFILE"), O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (NULL);
 	hist = sh_hist_gnl(fd);
 	close(fd);
-	if (!(new = sh_hline_new("", hist)))
+	if (!(new = sh_hline_new("", *hist)))
 		return (NULL);
-	if (hist)
+	if (*hist)
 	{
-		hist->down = new;
-		hist = hist->down;
+		(*hist)->down = new;
+		*hist = (*hist)->down;
 	}
 	else
-		hist = new;
-	return (hist);
+		*hist = new;
+	return (*hist);
 }
