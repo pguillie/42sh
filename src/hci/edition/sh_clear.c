b9a9d644ee4a8b12a86a270915118ecd173b88b3
pguillie <pguillie@student.42.fr>
2017-12-05 02:14:10 +0100

    FIX: sh_clear

--- src/hci/edition/sh_clear.c
+++ src/hci/edition/sh_clear.c
@@ -6,7 +6,7 @@
 /*   By: mdescamp <mdescamp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 20:53:10 by mdescamp          #+#    #+#             */
-/*   Updated: 2017/12/05 02:00:26 by mdescamp         ###   ########.fr       */
+/*   Updated: 2017/12/05 02:06:21 by pguillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int		sh_clear(t_line *line, t_coord **coord, t_tc tc)
 {
 	int		i;
-	int		l;
+	size_t	l;
 	t_coord	*new_coord;
 
 	tputs(tc.ce, 0, termput);
 	if (!(new_coord = sh_create_coord(line, tc.prompt)))
 		return (-1);
-	l = line->cur;
-	while ((*coord)[l].y != new_coord[line->used + 1].y
+	l = 0;
+	while (l <= line->used && (*coord)[l].y != new_coord[line->used + 1].y
 			&& (*coord)[l].x != new_coord[line->used + 1].x)
 		l++;
 	if ((*coord)[l - 1].y > new_coord[line->cur].y)
